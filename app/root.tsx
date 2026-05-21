6ce3c562a07ee876298ea64ef4b3e4641051ec2d
4sz5sz6sz <98520968+4sz5sz6sz@users.noreply.github.com>
2026-05-21 17:05:50 +0900

    fix: dangerouslySetInnerHTML biome-ignore 추가

--- app/root.tsx
+++ app/root.tsx
@@ -43,6 +43,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<meta content="width=device-width, initial-scale=1" name="viewport" />
 				{GTM_ID && (
 					<script
+						// biome-ignore lint/security/noDangerouslySetInnerHtml: GTM 공식 설치 스크립트
 						dangerouslySetInnerHTML={{
 							__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`,
 						}}
